e09b69c4265792b059b7f2ab6e78882b29368bcc
Barun Mitra <itbarunmitra1000@gmail.com>
2021-10-04 23:36:36 +0600

    create contact page

--- src/Components/Contact Us/ContactUs.js
+++ src/Components/Contact Us/ContactUs.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './Contact.css';
 const ContactUs = () => {
     return (
-        <div>
-            <h3>this is contact</h3>
+        <div className='contact-container'>
+        <h1 className= 'contact-header' >Contact Us</h1>
+            <h3 className='contact-text'>We Are Always here to Contact With You</h3>
+
+            <h4>Mobile: 017********</h4>
+            <h4 className='contact-gmail'>Gmail: example@gmail.com</h4>
         </div>
     );
 };
