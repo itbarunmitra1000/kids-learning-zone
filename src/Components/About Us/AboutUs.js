8ef366737e24282328ad011effdd8f8841d8a573
Barun Mitra <itbarunmitra1000@gmail.com>
2021-10-04 23:29:05 +0600

    creat about section

--- src/Components/About Us/AboutUs.js
+++ src/Components/About Us/AboutUs.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './About.css';
 
 const AboutUs = () => {
     return (
-        <div>
-            <h3>this is about us</h3>
+        <div className='about-container'>
+           <h1 className='about-header'>About Us</h1>
+           <p className='about-text'>Education is a devolved matter in the UK, with separate systems overseen by separate governments. England, Wales, and Northern Ireland, with certain exceptions, follow the National Curriculum of the United Kingdom. Scotland, on the other hand, has its own set of educational standards.
+            
+            We are always trying to better performance to build good communication and good mind set
+           </p>
         </div>
     );
 };
